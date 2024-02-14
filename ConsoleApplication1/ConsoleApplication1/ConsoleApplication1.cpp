74cc84397e91f7faa41dbb2ef9ed9c49f9cbf4a1
Ryutangtang <158537040+Ryutangtang@users.noreply.github.com>
2024-02-14 18:24:48 +0900

    Test

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Git Commit Test
+
+int main()
+{
+	const int Size_L = 20;
+	const int Size_W = 40;
+	char Arr_1[Size_L][Size_W];
+
+	for (int i = 0; i < Size_L; i++)
+	{
+		for (int j = 0; j < Size_W; j++)
+		{
+			Arr_1[i][j] = ' ';
+			if (i == 0 || j == 0 || i == 19 || j == 39)
+			{
+				Arr_1[i][j] = 'W';
+			}
+		}
+	}
+	for (int i = 0; i < Size_L; i++)
+	{
+		for (int j = 0; j < Size_W; j++)
+		{
+
+			cout << Arr_1[i][j];
+		}
+		cout << endl;
+	}
+
+}
